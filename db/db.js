09b300635134524a24ef43944601010d060fef6a
hhao <hhao@fiberhome.com>
2023-02-21 17:24:55 +0800

    reconnect db

--- db/db.js
+++ db/db.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://haohai:haohai@cluster0.xp0s2qx.mongodb.net/firstCloud?retryWrites=true&w=majority";
 mongoose.set('useCreateIndex', true);
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-})
-    .then(() => {
+const connect = () => {
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    }).then(() => {
         console.log('数据库连接成功')
-    }).catch((err) => {
-    console.log(err)
-    console.log('数据库连接失败')
-});
+    }).catch(err => {
+        console.log('数据库连接失败, 尝试重连...')
+        connect()
+    })
+}
+connect()
+
